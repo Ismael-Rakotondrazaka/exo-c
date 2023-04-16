c8f9e5e273a7589a49b4b936576aa845e0fb5140
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-04-16 19:59:31 +0300

    feat(exo8.c): add user input for value of val and print it in output message

--- src/TDAlgo3_4_2021.doc/exo8/exo8.c
+++ src/TDAlgo3_4_2021.doc/exo8/exo8.c
@@ -11,11 +11,15 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    char val[100];
+    printf("Type the value of val:\n");
+    scanf("%s", val);
+
     int firstRank = 0;
     int i = 1; // rank start from 1
     while (fscanf(f, "%s", &content) == 1)
     {
-        if (strcmp(content, "val") == 0)
+        if (strcmp(content, val) == 0)
         {
             firstRank = i;
             break;
@@ -24,7 +28,7 @@ int main(int argc, char **argv)
         i++;
     }
 
-    printf("firstRank = %d\n", firstRank);
+    printf("firstRank of %s = %d\n", val, firstRank);
     fclose(f);
     return 0;
 }
